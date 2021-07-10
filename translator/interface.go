52e4d251e491a2ce2b197ff07909c270106f4f94
amtoaer <amtoaer@gmail.com>
2021-07-10 16:24:23 +0800

    refactor: 精简代码

--- translator/interface.go
+++ translator/interface.go
@@ -17,13 +17,11 @@ type Translator interface {
 	Translate(content, fromLang, toLang string) string
 }
 
-type Mapper struct {
-	mapper map[string]string
-}
+type Mapper map[string]string
 
 func (m *Mapper) checkLang(fromLang, toLang string) (string, string, bool) {
-	validFromLang, isFromOk := m.mapper[fromLang]
-	validToLang, isToOk := m.mapper[toLang]
+	validFromLang, isFromOk := (*m)[fromLang]
+	validToLang, isToOk := (*m)[toLang]
 	return validFromLang, validToLang, isFromOk && isToOk
 }
 
@@ -33,20 +31,20 @@ func NewTranslator(m Method) (t Translator) {
 		return &baidu{token: utils.CheckEmpty(config.GlobalConf.BaiduToken, config.BAIDU_DEFAULT_TOKEN)}
 	case NIU:
 		return &niu{
-			Mapper: Mapper{map[string]string{
+			Mapper: map[string]string{
 				"zh": "zh",
 				"en": "en",
-			}},
+			},
 			apiKey: utils.CheckEmpty(config.GlobalConf.NiuToken, config.NIU_DEFAULT_TOKEN),
 		}
 	default:
 		return &youdao{
 			id:     utils.CheckEmpty(config.GlobalConf.YoudaoID, config.YOUDAO_DEFAULT_ID),
 			secret: utils.CheckEmpty(config.GlobalConf.YoudaoSecret, config.YOUDAO_DEFAULT_SECRET),
-			Mapper: Mapper{map[string]string{
+			Mapper: map[string]string{
 				"zh": "zh-CHS",
 				"en": "en",
-			}},
+			},
 		}
 	}
 }
